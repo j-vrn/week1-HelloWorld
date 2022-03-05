d7958f592f97839f68bbeb9c00370ed96d31a476
Julia Vrn <julia-vrn@hotmail.com>
2022-03-05 14:26:45 +0200

    add my first C# project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name?");
+            string userFirstName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            string userLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + userFirstName + " " + userLastName + "!");
+        }
+    }
+}
